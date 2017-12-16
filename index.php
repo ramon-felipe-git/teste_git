52db7e63e39698dc57a1cd20cf0862f91a67b95b
ramon-felipe <ramon.felipe@hotmail.com>
2017-12-16 00:32:01 -0200

    adding site files

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+?>
+
+<html>
+<head>
+	<title>Git/GitHub</title>
+</head>
+<link rel="icon" type="image/png" href="./img/favicon.png">
+<link rel="stylesheet" type="text/css" href="./css/style.css">
+
+<body>
+
+	<h1>teste</h1>
+</body>
+</html>
